ff3de486e89d14e91a7b787d3bd7860f68a8867d
Ryan Fitch <ryanfitch@mac.com>
2015-12-24 21:36:13 -0800

    pollished gallery

--- js/script.js
+++ js/script.js
@@ -1,19 +1,12 @@
 $(document).ready(function() {
-
   //menu slide down effect
   $(".dropdown-menu").hide();
   $(".dropdown-toggle").click(function() {
     $(".dropdown-menu").toggle(500);
-  });
-});
-
+	});
 //bio text slide in effect
-$(document).ready(function() {
   $(".bio-slide").hide().show(2000);
-});
-
 //contact text slide in effect
-$(document).ready(function() {
   $(".contact-slide").hide().show(2000);
 });
 
